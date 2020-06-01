b9610d1fab64f6d0828eb1bc9e90338e21dd82de
Nanjae <33223057+Nanjae@users.noreply.github.com>
2020-06-01 23:56:04 +0900

    Create Footer component

---
+++ src/Components/Footer.js
@@ -0,0 +1,319 @@
+import React from "react";
+import styled from "styled-components";
+import { useState } from "react";
+import ThemeDark from "../Styles/ThemeDark";
+
+const Wrapper = styled.div`
+  background-color: ${(props) => props.theme.bgFooterColor};
+  width: 100%;
+  height: fit-content;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Inner = styled.div`
+  width: 90%;
+  margin: 80px 30px 30px 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  @media only screen and (max-width: 575.99px) {
+    height: fit-content;
+  }
+  @media only screen and (min-width: 576px) {
+    height: 350px;
+  }
+  @media only screen and (min-width: 768px) {
+  }
+  @media only screen and (min-width: 992px) {
+  }
+  @media only screen and (min-width: 1200px) {
+  }
+  @media only screen and (min-width: 1536px) {
+  }
+  @media only screen and (min-width: 1800px) {
+  }
+`;
+
+const TopDiv = styled.div`
+  display: flex;
+  @media only screen and (max-width: 575.99px) {
+    width: fit-content;
+    justify-content: flex-start;
+    align-items: flex-start;
+    flex-direction: column;
+    height: 500px;
+  }
+  @media only screen and (min-width: 576px) {
+    width: 100%;
+    align-items: flex-start;
+    justify-content: flex-end;
+    flex-direction: row;
+    height: 70%;
+  }
+  @media only screen and (min-width: 768px) {
+  }
+  @media only screen and (min-width: 992px) {
+  }
+  @media only screen and (min-width: 1200px) {
+  }
+  @media only screen and (min-width: 1536px) {
+  }
+  @media only screen and (min-width: 1800px) {
+  }
+`;
+
+const TopBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media only screen and (max-width: 575.99px) {
+    width: fit-content;
+    align-items: flex-start;
+    height: 245px;
+    :not(:first-child) {
+      margin-left: 0px;
+    }
+  }
+  @media only screen and (min-width: 576px) {
+    width: 280px;
+    align-items: flex-start;
+    justify-content: space-between;
+    height: 100%;
+    :not(:first-child) {
+      margin-left: 80px;
+    }
+  }
+  @media only screen and (min-width: 768px) {
+  }
+  @media only screen and (min-width: 992px) {
+  }
+  @media only screen and (min-width: 1200px) {
+  }
+  @media only screen and (min-width: 1536px) {
+  }
+  @media only screen and (min-width: 1800px) {
+  }
+`;
+
+const TopMainTextBox = styled.div`
+  height: 30%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`;
+
+const TopMainText = styled.div`
+  color: ${(props) => props.theme.fontFooterColor};
+  font-size: 24px;
+  font-weight: 700;
+  :not(:first-child) {
+    margin-top: 5px;
+  }
+`;
+
+const TopSubTextBox = styled.div`
+  height: 60%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`;
+
+const TopSubText = styled.div`
+  color: ${(props) => props.theme.fontFooterColor};
+  font-size: 18px;
+  font-weight: 300;
+  display: flex;
+  align-items: center;
+  height: 25%;
+  width: fit-content;
+  transition: color 0.3s;
+`;
+
+const BottomDiv = styled.div`
+  width: 100%;
+  height: 25%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const BottomLeftBox = styled.div`
+  width: 65%;
+  height: 100%;
+  display: flex;
+  @media only screen and (max-width: 575.99px) {
+    justify-content: center;
+  }
+  @media only screen and (min-width: 576px) {
+    justify-content: flex-start;
+    align-items: flex-end;
+  }
+  @media only screen and (min-width: 768px) {
+  }
+  @media only screen and (min-width: 992px) {
+  }
+  @media only screen and (min-width: 1200px) {
+  }
+  @media only screen and (min-width: 1536px) {
+  }
+  @media only screen and (min-width: 1800px) {
+  }
+`;
+
+const BottomText = styled.div`
+  color: ${(props) => props.theme.lineSubColor};
+  text-align: center;
+  @media only screen and (max-width: 575.99px) {
+    font-size: 16px;
+  }
+  @media only screen and (min-width: 576px) {
+    font-size: 16px;
+    line-height: 20px;
+  }
+  @media only screen and (min-width: 768px) {
+  }
+  @media only screen and (min-width: 992px) {
+  }
+  @media only screen and (min-width: 1200px) {
+  }
+  @media only screen and (min-width: 1536px) {
+  }
+  @media only screen and (min-width: 1800px) {
+  }
+`;
+
+const BottomRightBox = styled.div`
+  width: 30%;
+  height: 100%;
+  display: flex;
+  @media only screen and (max-width: 575.99px) {
+    justify-content: center;
+  }
+  @media only screen and (min-width: 576px) {
+    justify-content: flex-end;
+    align-items: flex-end;
+  }
+  @media only screen and (min-width: 768px) {
+  }
+  @media only screen and (min-width: 992px) {
+  }
+  @media only screen and (min-width: 1200px) {
+  }
+  @media only screen and (min-width: 1536px) {
+  }
+  @media only screen and (min-width: 1800px) {
+  }
+`;
+
+export default () => {
+  const [textEnter, setTextEnter] = useState(0);
+
+  return (
+    <>
+      <Wrapper>
+        <Inner>
+          <TopDiv>
+            <TopBox>
+              <TopMainTextBox>
+                <TopMainText>Contact to</TopMainText>
+                <TopMainText>:: WATCHURS</TopMainText>
+              </TopMainTextBox>
+              <TopSubTextBox>
+                <TopSubText>개발 : 난재(Nanjae)</TopSubText>
+                <TopSubText
+                  onMouseEnter={() => {
+                    setTextEnter(1);
+                  }}
+                  onMouseLeave={() => {
+                    setTextEnter(0);
+                  }}
+                  style={
+                    textEnter === 1
+                      ? { color: ThemeDark.highlightColor, cursor: "pointer" }
+                      : { cursor: "pointer" }
+                  }
+                >
+                  dev.nanjae@gmail.com
+                </TopSubText>
+                <TopSubText
+                  onClick={() => {
+                    window.open(`https://watchurs.com`);
+                  }}
+                  onMouseEnter={() => {
+                    setTextEnter(4);
+                  }}
+                  onMouseLeave={() => {
+                    setTextEnter(0);
+                  }}
+                  style={
+                    textEnter === 4
+                      ? { color: ThemeDark.highlightColor, cursor: "pointer" }
+                      : { cursor: "pointer" }
+                  }
+                >
+                  프로젝트 와쳐스란?
+                </TopSubText>
+              </TopSubTextBox>
+            </TopBox>
+            <TopBox>
+              <TopMainTextBox>
+                <TopMainText>Services</TopMainText>
+                <TopMainText>:: Broadcaster</TopMainText>
+              </TopMainTextBox>
+              <TopSubTextBox>
+                <TopSubText
+                  onMouseEnter={() => {
+                    setTextEnter(2);
+                  }}
+                  onMouseLeave={() => {
+                    setTextEnter(0);
+                  }}
+                  style={
+                    textEnter === 2
+                      ? { color: ThemeDark.highlightColor, cursor: "pointer" }
+                      : { cursor: "pointer" }
+                  }
+                >
+                  리그 오브 레전드 : LOL
+                </TopSubText>
+                <TopSubText
+                  onClick={() => {
+                    window.open(`https://tft.watchurs.com`);
+                  }}
+                  onMouseEnter={() => {
+                    setTextEnter(3);
+                  }}
+                  onMouseLeave={() => {
+                    setTextEnter(0);
+                  }}
+                  style={
+                    textEnter === 3
+                      ? { color: ThemeDark.highlightColor, cursor: "pointer" }
+                      : { cursor: "pointer" }
+                  }
+                >
+                  전략적 팀 전투 : TFT
+                </TopSubText>
+              </TopSubTextBox>
+            </TopBox>
+          </TopDiv>
+          <BottomDiv>
+            <BottomLeftBox>
+              <BottomText>
+                WATCHURS isn't endorsed by Riot Games and doesn't reflect the
+                views or opinions of Riot Games or anyone officially involved in
+                producing or managing Riot Games properties. Riot Games, and all
+                associated properties are trademarks or registered trademarks of
+                Riot Games, Inc.
+              </BottomText>
+            </BottomLeftBox>
+            <BottomRightBox>
+              <BottomText>© 2020 WATCHURS. All rights reserved.</BottomText>
+            </BottomRightBox>
+          </BottomDiv>
+        </Inner>
+      </Wrapper>
+    </>
+  );
+};
--- src/Routes/Home/Home.js
+++ src/Routes/Home/Home.js
@@ -5,6 +5,8 @@ import ThemeLight from "../../Styles/ThemeLight";
 import Main from "../../Components/Main/Main";
 import useWindowDimensions from "../../Hooks/useWindowDimensions";
 import { useWindowScroll } from "@react-hook/window-scroll";
+import About from "../../Components/About/About";
+import Footer from "../../Components/Footer";
 
 const Wrapper = styled.div`
   /* overflow-y: hidden; */
@@ -31,106 +33,6 @@ const Inner = styled.div`
   }
 `;
 
-const AboutWrapper = styled.div`
-  position: relative;
-  width: 100%;
-  height: 850px;
-  background-color: ${(props) => props.theme.bgSubColor};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const AboutInner = styled.div`
-  width: 1050px;
-  height: 600px;
-  margin-top: 50px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`;
-
-const AboutTitleDiv = styled.div`
-  width: 100%;
-  height: 200px;
-  display: flex;
-  flex-direction: column;
-`;
-
-const AboutTitleBgText = styled.div`
-  position: absolute;
-  width: 1050px;
-  height: 300px;
-  font-size: 220px;
-  font-weight: 900;
-  letter-spacing: -10px;
-  opacity: 0.05;
-  display: flex;
-  align-items: center;
-`;
-
-const AboutTitleHeaderBox = styled.div`
-  width: 100%;
-  height: fit-content;
-  color: crimson;
-  opacity: 0.5;
-  margin-top: 30px;
-  margin-bottom: 20px;
-  font-weight: 700;
-`;
-
-const AboutTitleTextBox = styled.div`
-  width: 100%;
-  height: fit-content;
-`;
-
-const AboutTitleText = styled.div`
-  width: 100%;
-  height: fit-content;
-  font-size: 48px;
-  font-weight: 900;
-  margin-bottom: 15px;
-`;
-
-const AboutContentDiv = styled.div`
-  width: 100%;
-  height: 400px;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const AboutContentLeftDiv = styled.div`
-  width: 350px;
-  height: fit-content;
-  margin-top: 50px;
-`;
-
-const AboutContentLeftFeatureTitle = styled.div`
-  font-size: 24px;
-  font-weight: 900;
-  color: crimson;
-  margin-bottom: 40px;
-`;
-
-const AboutContentLeftFeatureText = styled.div`
-  margin-bottom: 20px;
-  font-weight: 500;
-`;
-
-const AbountContentRightDiv = styled.div`
-  width: 600px;
-  height: 100%;
-  margin-top: 50px;
-`;
-
-const AbountContentRightText = styled.div`
-  margin-left: 10px;
-  margin-right: 10px;
-  height: fit-content;
-  line-height: 40px;
-  color: gray;
-`;
-
 export default () => {
   const [siteTheme] = useState(false);
   // const [siteTheme, setSiteTheme] = useState(false);
@@ -150,50 +52,8 @@ export default () => {
               siteTheme={siteTheme}
               scrollY={scrollY}
             />
-            <AboutWrapper>
-              <AboutInner>
-                <AboutTitleDiv>
-                  <AboutTitleBgText>Watchurs</AboutTitleBgText>
-                  <AboutTitleHeaderBox>
-                    프로젝트 와쳐스 & TFT
-                  </AboutTitleHeaderBox>
-                  <AboutTitleTextBox>
-                    <AboutTitleText>
-                      여러분이 찾는 브로드캐스터의
-                    </AboutTitleText>
-                    <AboutTitleText>
-                      전략적 팀 전투 정보를 제공합니다.
-                    </AboutTitleText>
-                  </AboutTitleTextBox>
-                </AboutTitleDiv>
-                <AboutContentDiv>
-                  <AboutContentLeftDiv>
-                    <AboutContentLeftFeatureTitle>
-                      주요 기능
-                    </AboutContentLeftFeatureTitle>
-                    <AboutContentLeftFeatureText>
-                      브로드캐스터 소환사명 자동 업데이트
-                    </AboutContentLeftFeatureText>
-                    <AboutContentLeftFeatureText>
-                      브로드캐스터 랭크 정보 및 승률 조회
-                    </AboutContentLeftFeatureText>
-                  </AboutContentLeftDiv>
-                  <AbountContentRightDiv>
-                    <AbountContentRightText>
-                      와쳐스는 WATCH YOURS의 줄임말로 당신의 것, 즉, 일상생활 속
-                      생겨나는 당신의 관심사에 대한 정보를 제공하기 위해
-                      만들어졌습니다. 각각의 정보들은 와쳐스라는 하나의 프로젝트
-                      속에서 다양한 스타일과 기술을 통해 만나보실 수 있습니다.
-                      현재는 브로드캐스터와 관련된 게임 정보 위주로 제작 중이며,
-                      언젠가 범위를 넓혀 사소하지만 가끔은 여러분의 관심을 끄는
-                      정보들을 한눈에 보여줄 수 있도록 기획 중입니다. 원하시는
-                      정보나 아이디어, 문의가 있으시면 사이트 하단의 이메일로
-                      연락해주시면 감사하겠습니다.
-                    </AbountContentRightText>
-                  </AbountContentRightDiv>
-                </AboutContentDiv>
-              </AboutInner>
-            </AboutWrapper>
+            <About />
+            <Footer />
           </Inner>
         </Wrapper>
       </ThemeProvider>
