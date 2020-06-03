e4e6b5ce1772823ac3f2b10a415ec6dc395a691b
Nanjae <33223057+Nanjae@users.noreply.github.com>
2020-06-03 19:46:46 +0900

    Update Client.js

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -4,5 +4,5 @@ export default new ApolloClient({
   uri:
     process.env.NODE_ENV === "development"
       ? "http://localhost:4000"
-      : "https://watchurs-backend-dev.herokuapp.com/",
+      : process.env.APOLLO_URI,
 });
